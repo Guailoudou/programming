fb2407333c67806ffa74ddb02eea5c0a9528b730
Guailoudou <108342285+Guailoudou@users.noreply.github.com>
2023-02-23 14:15:55 +0800

    Add files via upload

--- Python/自动登录校园网.py
+++ Python/自动登录校园网.py
@@ -1,4 +1,4 @@
-import requests,os,json
+import requests,os,json,time
 url = "http://172.17.3.2/login?id=1"
 jsonwj = os.path.exists("wic-config.json")
 
@@ -20,13 +20,31 @@ def login():
         key = json.load(f)
     myobj = {'username': key['username'],'password': key['password']}
     x = requests.post(url, data = myobj)
-    n = x.text.find("登录成功")
-    if n == -1:
+    n1 = x.text.find("登录成功")
+    n2 = x.text.find("nginx") #过载
+    n3 = x.text.find("异常") #异常代理
+    n4 = x.text.find("账号/密码错误") #密码错误
+    n5 = x.text.find("代理认证失败") #账户错误
+    if n4 != -1 | n5 != -1:
         print("登录失败，请检查账户密码，下面可以重新设置账户密码，设置完之后重新启动")
         wjxt()
         login()
-    else:
+    elif n2 != -1 :
+        print("登录服务器过载，将在2s后再次尝试登录")
+        time.sleep(2)
+        login()
+    elif n1 != -1:
         print("登录成功")
+    elif n3 != -1 :
+        print("出现异常代理行为，账户被临时封禁，将在5min后尝试重新连接")
+        time.sleep(300)
+        login()
+    else:
+        print("出现了意料之外的结果,开始导出错误反馈日志")
+        open('web-err.log','w').write(x.text)
+        print("导出完毕，请将web-err.log文件提交给我，这有助于我修复这个问题，文件在程序本体位置")
+        print("我的联系方式--QQ：124193334 Email：Guailoudou@163.com")
+        os.system("pause")
 
 net = requests.get(url)
 netlog = net.text.find("登录成功")
