973a1a5e7a797b475dfe43b4c43889c884fad6d8
Guailoudou <108342285+Guailoudou@users.noreply.github.com>
2023-05-10 10:11:31 +0800

    Update connect.php

--- html/Ly/connect.php
+++ html/Ly/connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "qwertyuiop";
+$username = "*****";
+$password = "*****";
 $dbname = "web";
 $connect=mysqli_connect($servername, $username, $password, $dbname);//连接数据库
 global $connect;
